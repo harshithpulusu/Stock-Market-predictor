27b504873ee322f943846142139322baea7d9c4c
harshithpulusu <harshithpulusu@gmail.com>
2025-08-13 17:25:06 -0700

    Backups and add-ons

---
+++ setup_nltk.py
@@ -0,0 +1,13 @@
+import nltk
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+# Download required NLTK data
+nltk.download('vader_lexicon')
+print("NLTK data downloaded successfully!")
